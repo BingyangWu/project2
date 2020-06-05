1fb0714ba1d60c2600fa1c739d7a7f657cacf546
QAeQ <43445007+QAeQ@users.noreply.github.com>
2020-06-05 18:28:36 +0800

    Add files via upload

---
+++ include/GenerateAST.h
@@ -0,0 +1,28 @@
+#ifndef BOOST_GENERATEAST_H
+#define BOOST_GENERATEAST_H
+
+#include <string>
+#include "IR.h"
+#include <set>
+
+using namespace Boost::Internal;
+
+class Expr_compare{
+	public:
+		bool operator()(const Expr &a,const Expr &b) const {
+			return a.real_ptr()<b.real_ptr();
+		}
+	   
+};
+
+class mykernel{
+public:
+    Group kernel;
+    std::set<Expr,Expr_compare> right_expr;
+    std::vector<String> grad;
+    mykernel(Group _kernel,std::set<Expr,Expr_compare> _right_expr,std::vector<String> _grad) : kernel(_kernel), right_expr(_right_expr),grad(_grad) {}
+};
+
+mykernel generateAST(std::string filename);
+
+#endif
